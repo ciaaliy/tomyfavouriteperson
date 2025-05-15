e1f5ec427062174de38fd83a6eae4f3c8dce9586
ciaaliy <aliciayap2504@gmail.com>
2025-05-15 13:11:16 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>hi eli</h1>
+      <h2>here's a little but really important question for you</h2>
+    </div>
+  );
+}
